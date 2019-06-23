00555f840c6c95d07450731b50e637184aaf5bd7
realEricBrun <ericbrun350@att.net>
2019-06-23 14:54:02 -0500

    Integrated 32-bit SFML library for graphics.

--- VSGitHelloWorld/VSGitHelloWorld/main.cpp
+++ VSGitHelloWorld/VSGitHelloWorld/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <string>
+#include <SFML/Main.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 
 using std::cout;
 using std::cin;
@@ -16,8 +21,25 @@ void pause()
 
 int main()
 {
-	cout << "Hello world!" << endl;
-	pause();
+	string msg = "Hello world!";
+	cout << msg << endl;
+	
+	//create a window
+	sf::RenderWindow mainWindow(sf::VideoMode(800, 600), "VSGitHelloWorld");
+
+	while (mainWindow.isOpen())
+	{
+		sf::Event sfE;
+		while (mainWindow.pollEvent(sfE))
+		{
+			if(sfE.type == sf::Event::EventType::Closed)
+			{
+				mainWindow.close();
+			}
+		}
+	}
+	
+	//pause();
 
 	return 0;
 }
